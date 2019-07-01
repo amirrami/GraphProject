eb2fe5a24a01cc928a076aa44666a06df6419f88
andrew_morcos <36261350+Andro-Morcos-Nagib@users.noreply.github.com>
2019-07-01 11:40:56 +0200

    Degree Centrality
    c++ code

---
+++ C++ codes/degee_centrality.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+#include <cctype>
+#include <cstdlib>
+#include<map>
+
+#include <iostream>
+#include<fstream>
+
+using namespace std;
+
+int main()
+{
+	
+	int Number_nodes, Number_edges;
+	ifstream my_file("example.txt");
+	if (my_file.is_open())
+	{
+		my_file >> Number_nodes >> Number_edges;
+		vector<vector<pair<int, int> > > adj_list(Number_nodes, vector<pair<int, int> >());
+		
+		int a, b, c;
+
+		for (int i = 0; i < Number_edges; i++)
+		{
+			my_file >> a >> b >> c;
+			adj_list[a].push_back({ b, c });
+			adj_list[b].push_back({ a, c });
+
+
+		}
+
+		for (int i = 0; i < Number_nodes; i++)
+		{
+			cout <<adj_list[i].size() <<endl ;
+
+		}
+
+	
+	
+
+	}
+	else
+		cout << "file not opened" << endl;
+
+	my_file.close();
+	//system("pause");
+	return 0;
+}
